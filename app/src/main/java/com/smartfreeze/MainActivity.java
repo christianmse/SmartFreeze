0a5f941b44f2ab83b91c68230f707b171e7cf818
christianmse <christianmsepinoza@gmail.com>
2020-03-11 20:35:48 +0100

    FrameLayout fixed

--- app/src/main/java/com/smartfreeze/MainActivity.java
+++ app/src/main/java/com/smartfreeze/MainActivity.java
@@ -2,11 +2,15 @@ package com.smartfreeze;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -18,7 +22,7 @@ import com.smartfreeze.ui.bottom_menu.TiendaOpcion;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements IPrincipalListener {
-
+    RelativeLayout notificacionesLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements IPrincipalListene
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout_main, new PrincipalOpcion()).commit();
         }
-
+        notificacionesLayout = findViewById(R.id.notificaciones_layout);
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
@@ -41,15 +45,18 @@ public class MainActivity extends AppCompatActivity implements IPrincipalListene
                     Fragment fragment = null;
                     switch (item.getItemId()){
                         case R.id.navigation_ajustes:
+                           notificacionesLayout.setVisibility(View.GONE);
                             fragment = new AjustesOpcion();
                             break;
 
                         case R.id.navigation_principal:
                             fragment = new PrincipalOpcion();
+                            notificacionesLayout.setVisibility(View.VISIBLE);
                             break;
 
                         case R.id.navigation_tienda:
                             fragment = new TiendaOpcion();
+                            notificacionesLayout.setVisibility(View.GONE);
                             break;
                     }
 
