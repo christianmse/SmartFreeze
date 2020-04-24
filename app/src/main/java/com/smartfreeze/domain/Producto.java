435a789237d88a82c40fd126ea52110fb4750ad1
christianmse <christianmsepinoza@gmail.com>
2020-04-25 01:47:58 +0200

    pass data entre storeFragment y paymarket

--- app/src/main/java/com/smartfreeze/domain/Producto.java
+++ app/src/main/java/com/smartfreeze/domain/Producto.java
@@ -1,6 +1,9 @@
 package com.smartfreeze.domain;
 
-public class Producto {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Producto implements Parcelable {
 
     private String nombre;
     private String categoria;
@@ -55,4 +58,41 @@ public class Producto {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
+
+
+    protected Producto(Parcel in) {
+        nombre = in.readString();
+        categoria = in.readString();
+        precio = in.readString();
+        drawable = in.readInt();
+        descripcion = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(nombre);
+        dest.writeString(categoria);
+        dest.writeString(precio);
+        dest.writeInt(drawable);
+        dest.writeString(descripcion);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Producto> CREATOR = new Parcelable.Creator<Producto>() {
+        @Override
+        public Producto createFromParcel(Parcel in) {
+            return new Producto(in);
+        }
+
+        @Override
+        public Producto[] newArray(int size) {
+            return new Producto[size];
+        }
+    };
 }
